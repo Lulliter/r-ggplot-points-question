d8d855122f21dd22c3b87f8c06d754af0ea515c8
Lulliter <lmm76@georgetown.edu>
2018-10-24 16:53:34 -0400

    rds

--- ggplot_BUBBLEMAP_questions.R
+++ ggplot_BUBBLEMAP_questions.R
@@ -1,12 +1,6 @@
+# data available here 
+https://github.com/Lulliter/r-ggplot-points-question
 
-save(Brazil_bound_sf, file = "./100_Extra/Brazil_bound_sf.Rdata")
-save(Brazil_cit_longlat, file = "./100_Extra/Brazil_cit_longlat.Rdata")
-save(Brazil_cit_sf, file = "./100_Extra/Brazil_cit_sf.Rdata")
-
-
-
-class(Brazil_cit_longlat$"2016") # numeric
-is.na(Brazil_cit_longlat$"2016") # 0 missing
 
 # Plot does not Work
 gg_bra2_2016 <- ggplot() + 
@@ -16,10 +10,6 @@ gg_bra2_2016 <- ggplot() +
 	scale_size_continuous()  +
 	
 	coord_sf(crs = st_crs(Brazil_bound_sf), datum = NA) + 
-	# labs(x = NULL, y = NULL,
-	#     title = "Cities in LAC region",
-	#     subtitle = "population in 2016" ,
-	#     caption  = paste("Sources: UN", "\n","Prepared on", format(Sys.Date(), format="%d %B %Y"))) +
 	theme_void() 
 
 gg_bra2_2016
